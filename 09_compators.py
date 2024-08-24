4a2b40357b76ebab3c436340b1e7d00956614a51
Harold Navarro <kaendor50@gmail.com>
2024-08-23 19:46:55 -0600

    archivo 09 de comparaciones añadidas

---
+++ 09_compators.py
@@ -0,0 +1,43 @@
+# > mayor que
+print("Mayor que")
+print(f"4 > 3 : {4 > 3}")
+print(f"1 > 10: {1 > 10}")
+print(f"10 >10: {10 > 10}\n")
+
+# < menor que
+print("Menor que")
+print(f"2 < 8 : {2 < 8}")
+print(f"10 < 5: {10 < 5}")
+print(f"3 < 3 : {3 < 3} \n")
+
+# >= mayor o igual que
+print("Mayor o igual que")
+print(f"5 >= 2: {5 >= 2}")
+print(f"6 >= 9: {6 >= 9}")
+print(f"4 >= 4: {4 >= 4}\n")
+
+# <= menor o igual que
+print("Menor o igual que")
+print(f"5 <= 2: {5 <= 2}")
+print(f"6 <= 5: {6 <= 5}")
+print(f"6 <= 6: {6 <= 6}\n")
+
+# == igual que
+print("Igual que")
+print(f"1 == 4: {1 == 4}")
+print(f"5 == 7: {5 == 7}")
+print(f"2 == 2: {2 == 2}\n")
+
+# != diferente que
+print("Diferente que")
+print(f"6 != 10: {6 != 10}")
+# Con strings
+print(f"\"Apple\" == 'Apple': {"Apple" == 'Apple'}")
+print(f"\"Apple\" == 'apple': {"Apple" == 'apple'}")
+# Con diferentes tipos de variable 
+print(f"\"1\" == 1: {"1" == 1}")
+
+# ¿Mayor de edad?
+age = 18
+print("Definimos nuestra edad en la variable: \nage = 18")
+print(f"age >= 18: {age >= 18}")
