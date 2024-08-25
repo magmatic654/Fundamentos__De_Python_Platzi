7eceb25366969d608d944e71623ce79aa4dc975f
Harold Navarro <kaendor50@gmail.com>
2024-08-25 14:46:20 -0600

    Cambiando main por juego de piedra papel o tijera

--- main.py
+++ main.py
@@ -1,21 +1,50 @@
-print("Hola, esto es python")
+import random
 
-print("Hola, soy Harold y tengo 19 años")
+def computerRandom():
+    option = options(randomNumber(3))
+    return option
 
-print("Hola", 12+5)
-print(10-5)
-print(5*5)
-print(8/2)
+def randomNumber(max):
+    return random.randint(1, max)
 
-#esto es un comentario
 
-"""
-varias
-lineas
-otra
-"""
+def options(random_number):
+    if random_number == 1:
+        return 'piedra'
+    elif random_number == 2:
+        return 'papel'
+    else:
+        return 'tijera'
 
-'''
-varias 
-lineas
-'''
+user_option = input('Elije piedra, papel o tijera => ').lower()
+computer_option = computerRandom()
+
+def winMessage():
+    print(f'{user_option} gana a {computer_option}')
+    print('Has ganado')
+
+def loseMessage():
+    print(f'{computer_option} gana a {user_option}')
+    print('Has perdido')
+
+def drawMessage():
+    print(f'{user_option} empata con {computer_option}')
+    print('Empate')
+
+if user_option == computer_option:
+    drawMessage()
+elif user_option == 'piedra':
+    if computer_option == 'tijera':
+        winMessage()
+    else:
+        loseMessage()
+elif user_option == 'papel':
+    if computer_option == 'piedra':
+        winMessage()
+    else:
+        loseMessage()
+elif user_option == 'tijera':
+    if computer_option == 'papel':
+        winMessage()
+    else:
+        loseMessage()
