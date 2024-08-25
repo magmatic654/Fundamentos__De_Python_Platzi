f1cfdf69e25bffe16604c8800aeb92b2dc7d6545
Harold Navarro <kaendor50@gmail.com>
2024-08-24 18:28:49 -0600

    Agregando archivo reto par o impar

---
+++ 13_odd_or_even.py
@@ -0,0 +1,9 @@
+# Reto crear un programa que suelte si un 
+# número es par o impar.
+
+user_number = int(input("Dame un numero entero, el que quieras => "))
+
+if user_number % 2 == 0:
+    print("El número es par")
+else:
+    print("El número es impar")
