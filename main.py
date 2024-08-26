7a7f4a972cd373591f00116f7253328ab685d42e
Harold Navarro <kaendor50@gmail.com>
2024-08-25 19:46:26 -0600

    Añadiendo comentarios descriptivos en  main

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import random
 
+# retorna la opción aleatoria de la maquina
 def computerRandom():
     option = options(randomNumber(3))
     return option
 
+# retorna un número aleatorio dentro de un rango del 1
+# al número que elijas
 def randomNumber(max):
     return random.randint(1, max)
 
-
+# Pasa los números a la opción del juego (piedra, papel, tijera)
 def options(random_number):
     if random_number == 1:
         return 'piedra'
@@ -19,6 +22,7 @@ def options(random_number):
 user_option = input('Elije piedra, papel o tijera => ').lower()
 computer_option = computerRandom()
 
+# Mensajes de estados del juego
 def winMessage():
     print(f'{user_option} gana a {computer_option}')
     print('Has ganado')
@@ -31,6 +35,7 @@ def drawMessage():
     print(f'{user_option} empata con {computer_option}')
     print('Empate')
 
+# Lógica del juego
 if user_option == computer_option:
     drawMessage()
 elif user_option == 'piedra':
